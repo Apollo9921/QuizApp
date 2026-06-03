a64747d9a190e2cf1e96e8aec84d63f6519ed610
DESKTOP-TBTK50T\Nuno Silva <nunosilvaw33d@gmail.com>
2026-06-03 10:29:54 +0100

    clean up serialization dependencies and update versions

--- app/src/main/java/com/example/quizapp/data/network/dto/QuestionDTO.kt
+++ app/src/main/java/com/example/quizapp/data/network/dto/QuestionDTO.kt
@@ -1,9 +1,7 @@
 package com.example.quizapp.data.network.dto
 
-import kotlinx.serialization.InternalSerializationApi
 import kotlinx.serialization.Serializable
 
-@OptIn(InternalSerializationApi::class)
 @Serializable
 data class QuestionDTO(
     val text: String
