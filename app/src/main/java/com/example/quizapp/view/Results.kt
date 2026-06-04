dd8ba6efd442369364ed5519b4c6b9cd21e8c9f2
DESKTOP-TBTK50T\Nuno Silva <nunosilvaw33d@gmail.com>
2026-06-04 11:12:12 +0100

    refactor pie chart components from custom functions to results view

--- app/src/main/java/com/example/quizapp/view/Results.kt
+++ app/src/main/java/com/example/quizapp/view/Results.kt
@@ -1,20 +1,38 @@
 package com.example.quizapp.view
 
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
@@ -24,8 +42,17 @@ import com.example.quizapp.data.local.entity.ResultsEntity
 import com.example.quizapp.data.local.entity.UserEntity
 import com.example.quizapp.view.bottomBar.BottomNavigationBar
 import com.example.quizapp.view.custom.*
+import com.example.quizapp.view.theme.Black
+import com.example.quizapp.view.theme.Blue
+import com.example.quizapp.view.theme.DarkGreen
+import com.example.quizapp.view.theme.Green
+import com.example.quizapp.view.theme.Orange
+import com.example.quizapp.view.theme.Pink40
+import com.example.quizapp.view.theme.Purple40
 import com.example.quizapp.view.theme.PurpleGrey40
+import com.example.quizapp.view.theme.Red
 import com.example.quizapp.view.theme.White
+import com.example.quizapp.view.theme.Yellow
 
 private lateinit var user: SnapshotStateList<UserEntity>
 private lateinit var results: SnapshotStateList<ResultsEntity>
@@ -154,5 +181,166 @@ private fun ShowResults() {
         PieChart(
             data = data
         )
+    }
+}
+
+@Composable
+private fun PieChart(
+    data: Map<String, Int>,
+    radiusOuter: Dp = if (mediaQueryWidth() <= small) {
+        70.dp
+    } else if (mediaQueryWidth() <= normal) {
+        100.dp
+    } else {
+        130.dp
+    },
+    chartBarWidth: Dp = if (mediaQueryWidth() <= small) {
+        15.dp
+    } else if (mediaQueryWidth() <= normal) {
+        25.dp
+    } else {
+        35.dp
+    },
+    animDuration: Int = 1000,
+) {
+
+    val totalSum = data.values.sum()
+    val floatValue = mutableListOf<Float>()
+
+    data.values.forEachIndexed { index, values ->
+        floatValue.add(index, 360 * values.toFloat() / totalSum.toFloat())
+    }
+
+    val colors = listOf(
+        Purple40,
+        Pink40,
+        White,
+        Black,
+        Red,
+        Yellow,
+        DarkGreen,
+        Green,
+        Blue,
+        Orange
+    )
+
+    var animationPlayed by remember { mutableStateOf(false) }
+
+    var lastValue = 0f
+
+    val animateSize by animateFloatAsState(
+        targetValue = if (animationPlayed) radiusOuter.value * 2f else 0f,
+        animationSpec = tween(
+            durationMillis = animDuration,
+            delayMillis = 0,
+            easing = LinearOutSlowInEasing
+        ), label = ""
+    )
+
+    val animateRotation by animateFloatAsState(
+        targetValue = if (animationPlayed) 90f * 11f else 0f,
+        animationSpec = tween(
+            durationMillis = animDuration,
+            delayMillis = 0,
+            easing = LinearOutSlowInEasing
+        ), label = ""
+    )
+
+    LaunchedEffect(key1 = true) {
+        animationPlayed = true
+    }
+
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(
+            modifier = Modifier.size(animateSize.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Canvas(
+                modifier = Modifier
+                    .size(radiusOuter * 2f)
+                    .rotate(animateRotation)
+            ) {
+                floatValue.forEachIndexed { index, value ->
+                    drawArc(
+                        color = colors[index],
+                        lastValue,
+                        value,
+                        useCenter = false,
+                        style = Stroke(chartBarWidth.toPx(), cap = StrokeCap.Butt)
+                    )
+                    lastValue += value
+                }
+            }
+        }
+    }
+    DetailsPieChart(
+        data = data,
+        colors = colors
+    )
+}
+
+@Composable
+fun DetailsPieChart(
+    data: Map<String, Int>,
+    colors: List<Color>
+) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 20.dp)
+    ) {
+        items(data.size) { item ->
+            DetailsPieChartItem(
+                data = Pair(data.keys.elementAt(item), data.values.elementAt(item)),
+                color = colors[item]
+            )
+        }
+    }
+}
+
+@Composable
+fun DetailsPieChartItem(
+    data: Pair<String, Int>,
+    height: Dp = if (mediaQueryWidth() <= small) {
+        45.dp
+    } else if (mediaQueryWidth() <= normal) {
+        55.dp
+    } else {
+        65.dp
+    },
+    color: Color
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 10.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .background(
+                    color = color,
+                    shape = RoundedCornerShape(10.dp)
+                )
+                .size(height)
+        )
+        Column(modifier = Modifier.fillMaxWidth()) {
+            Text(
+                style = MaterialTheme.typography.labelSmall,
+                modifier = Modifier.padding(start = 15.dp),
+                text = data.first,
+                color = Black,
+                textAlign = TextAlign.Start
+            )
+            Text(
+                style = MaterialTheme.typography.labelSmall,
+                modifier = Modifier.padding(start = 15.dp),
+                text = data.second.toString() + "%",
+                color = White
+            )
+        }
     }
 }
--- app/src/main/java/com/example/quizapp/view/custom/CustomFunctions.kt
+++ app/src/main/java/com/example/quizapp/view/custom/CustomFunctions.kt
@@ -1,37 +1,19 @@
 package com.example.quizapp.view.custom
 
 import android.view.WindowManager
-import androidx.compose.animation.core.LinearOutSlowInEasing
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.quizapp.R
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.StrokeCap
-import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.sp
 import com.example.quizapp.view.theme.*
 import kotlin.math.ln
 import kotlin.math.pow
@@ -108,183 +90,6 @@ fun formatTotalCount(count: Float): String {
         count / 1000.0.pow(exp.toDouble()),
         "kMGTPE"[exp - 1]
     )
-}
-
-@Composable
-fun PieChart(
-    data: Map<String, Int>,
-    radiusOuter: Dp = if (mediaQueryWidth() <= small) {
-        70.dp
-    } else if (mediaQueryWidth() <= normal) {
-        100.dp
-    } else {
-        130.dp
-    },
-    chartBarWidth: Dp = if (mediaQueryWidth() <= small) {
-        15.dp
-    } else if (mediaQueryWidth() <= normal) {
-        25.dp
-    } else {
-        35.dp
-    },
-    animDuration: Int = 1000,
-) {
-
-    val totalSum = data.values.sum()
-    val floatValue = mutableListOf<Float>()
-
-    data.values.forEachIndexed { index, values ->
-        floatValue.add(index, 360 * values.toFloat() / totalSum.toFloat())
-    }
-
-    val colors = listOf(
-        Purple40,
-        Pink40,
-        White,
-        Black,
-        Red,
-        Yellow,
-        DarkGreen,
-        Green,
-        Blue,
-        Orange
-    )
-
-    var animationPlayed by remember { mutableStateOf(false) }
-
-    var lastValue = 0f
-
-    val animateSize by animateFloatAsState(
-        targetValue = if (animationPlayed) radiusOuter.value * 2f else 0f,
-        animationSpec = tween(
-            durationMillis = animDuration,
-            delayMillis = 0,
-            easing = LinearOutSlowInEasing
-        ), label = ""
-    )
-
-    val animateRotation by animateFloatAsState(
-        targetValue = if (animationPlayed) 90f * 11f else 0f,
-        animationSpec = tween(
-            durationMillis = animDuration,
-            delayMillis = 0,
-            easing = LinearOutSlowInEasing
-        ), label = ""
-    )
-
-    LaunchedEffect(key1 = true) {
-        animationPlayed = true
-    }
-
-    Column(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Box(
-            modifier = Modifier.size(animateSize.dp),
-            contentAlignment = Alignment.Center
-        ) {
-            Canvas(
-                modifier = Modifier
-                    .size(radiusOuter * 2f)
-                    .rotate(animateRotation)
-            ) {
-                floatValue.forEachIndexed { index, value ->
-                    drawArc(
-                        color = colors[index],
-                        lastValue,
-                        value,
-                        useCenter = false,
-                        style = Stroke(chartBarWidth.toPx(), cap = StrokeCap.Butt)
-                    )
-                    lastValue += value
-                }
-            }
-        }
-    }
-    DetailsPieChart(
-        data = data,
-        colors = colors
-    )
-}
-
-@Composable
-fun DetailsPieChart(
-    data: Map<String, Int>,
-    colors: List<Color>
-) {
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(2),
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(top = 20.dp)
-    ) {
-        items(data.size) { item ->
-            DetailsPieChartItem(
-                data = Pair(data.keys.elementAt(item), data.values.elementAt(item)),
-                color = colors[item]
-            )
-        }
-    }
-}
-
-@Composable
-fun DetailsPieChartItem(
-    data: Pair<String, Int>,
-    height: Dp = if (mediaQueryWidth() <= small) {
-        45.dp
-    } else if (mediaQueryWidth() <= normal) {
-        55.dp
-    } else {
-        65.dp
-    },
-    color: Color
-) {
-    Row(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(top = 10.dp)
-    ) {
-        Box(
-            modifier = Modifier
-                .background(
-                    color = color,
-                    shape = RoundedCornerShape(10.dp)
-                )
-                .size(height)
-        )
-        Column(modifier = Modifier.fillMaxWidth()) {
-            Text(
-                modifier = Modifier.padding(start = 15.dp),
-                text = data.first,
-                fontWeight = FontWeight.Medium,
-                fontSize =
-                if (mediaQueryWidth() <= small) {
-                    20.sp
-                } else if (mediaQueryWidth() <= normal) {
-                    30.sp
-                } else {
-                    40.sp
-                },
-                color = Black
-            )
-            Text(
-                modifier = Modifier.padding(start = 15.dp),
-                text = data.second.toString() + "%",
-                fontWeight = FontWeight.Medium,
-                fontSize =
-                if (mediaQueryWidth() <= small) {
-                    20.sp
-                } else if (mediaQueryWidth() <= normal) {
-                    30.sp
-                } else {
-                    40.sp
-                },
-                color = White,
-                textAlign = TextAlign.Center
-            )
-        }
-    }
 }
 
 @Composable
